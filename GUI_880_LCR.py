f2af01603a3a452f1eb3e1dba500a73965a37441
andy <5129456+pandypanda@users.noreply.github.com>
2019-03-11 02:36:54 +0100

    Add files via upload

--- GUI_880_LCR.py
+++ GUI_880_LCR.py
@@ -1,9 +1,25 @@
- 
-import sys
-from itertools import product
-from PyQt5.QtWidgets import QApplication, QFrame, QGridLayout, QHBoxLayout, QPushButton, QSizePolicy, QSpacerItem, QToolButton, QVBoxLayout, QWidget
-#from PyQt5.QtCore import pyqtSlot
+#############################################################################
+#               Andy's test for BK Precision LCR Meter 880                  #
+#               Using SCPI command thru Python with VISA module             #
+#               Visit https://pyvisa.readthedocs.io for install             #
+#############################################################################
+# Com negociated/conf by VISA                                               #
+# Baudrate : 9600           Data bits : 8           Flow Control : None     #
+# Parity   : None           Stop bits : 1                                   #
+# Host returning a result after a querry command : <Result> + <CR> <LF>     #
+#############################################################################
+# GUI - Computing values and convert, format                                #
+#       to display mesured values properly                                  #
+#       Display status and config of the instrument                         #
+#############################################################################
 
+import sys
+
+from itertools import product
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QFrame, QLCDNumber, QGridLayout, QLineEdit, QHBoxLayout, QPushButton, QSizePolicy, QSpacerItem, QToolButton, QVBoxLayout, QWidget
+
+#from BK_880_LCR import *
 
 
 class Widget(QWidget):
@@ -14,6 +30,7 @@ class Widget(QWidget):
 
     def layoutUI(self):
         self.setStyleSheet("background-color: purple;")
+        self.setWindowTitle('BK Precision - 880 LCR')
 
         self.principalLayout = QHBoxLayout(self)
 
@@ -22,6 +39,13 @@ class Widget(QWidget):
         self.rightFrame.setFrameShadow(QFrame.Raised)
         self.verticalLayout = QVBoxLayout(self.rightFrame)
         self.gridLayout = QGridLayout()
+
+        # Affichage LCD PRIMARY
+        self.lcdPRI = QLCDNumber()
+        self.gridLayout.addWidget(self.lcdPRI, 0, 0, 2, 0)
+        # Affichage LCD SECONDARY
+        self.lcdSEC = QLCDNumber()
+        self.gridLayout.addWidget(self.lcdSEC, 2, 0, 4, 0)
 
         btns = {(0, 0): "L", (0, 1): "C", (0, 2): "R", (0, 3): "Z", (0, 4): "DCR",
                 (1, 0): "D", (1, 1): "Q", (1, 3): "THETA", (1, 4): "ESR",
@@ -33,11 +57,14 @@ class Widget(QWidget):
             x, y = pos
             btn = QPushButton(self.rightFrame)
             btn.setText(name)
-            self.gridLayout.addWidget(btn, x, y)
+            self.gridLayout.addWidget(btn, x+6, y)      # x+4 = hauteur affichages LCDs
             # When btn is clicked, send his 'name' to 'make_callfunction'
             btn.clicked.connect(self.make_callfunction(name))
         self.verticalLayout.addLayout(self.gridLayout)
         self.principalLayout.addWidget(self.rightFrame)
+        # Display watever --> QLCDNumber a config pour l'affichage complet :
+        self.lcdPRI.display(-1.23)    # Format 6 digits +exp (p, n, u, m, unit)
+        self.lcdSEC.display(-4.56)    # Format (PRI: +6.74095e-13 SEC: +2.52358e-02)
 
     # Print the 'name' of the clicked btn inthe console
     def make_callfunction(self, name):
